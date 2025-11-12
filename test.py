fe556c684508d266398f8b43ee1c8dd161756db4
Leo <leonardodguzman11@gmail.com>
2025-11-12 14:47:16 -0600

    Finished the initial script test, next is starting to look into options for formatting with html. Had to move files around to avoid leaking refresh token or credentials

--- test.py
+++ test.py
@@ -29,23 +29,29 @@ def getGmailCredentials():
     return creds
     
 
-email = input("Type your Email: ") # Sender's email address
-receiver_email = input("Type the recipient's Email: ") # Receiver's email address; eventually to be a list using pandas
+
+sender_email = input("Type your Email Address: ") # Sender's email address
+receiver_email = input("Type the recipient's Email Address: ") # Receiver's email address; eventually to be a list using pandas
+
 
 subject = input("Please type the subject of the email: ") #Subject of email
-message = input("Please type the contents of the email: ") #Contents of email
+message_body = input("Please type the contents of the email: ") #Contents of email
 
 creds = getGmailCredentials() #Logs into gmail
-service = build('gmail', 'v1', credentials=creds) #connects to gmail
+service = build('gmail', 'v1', credentials=creds) #connects to
 
-text = f"Subject: {subject}\n\n{message}" #fstring allows for special formatting
+#body of email
+message = MIMEText(message_body)
+message['to'] = receiver_email
+message['subject'] = subject
+encoded_message = base64.urlsafe_b64encode(message.as_bytes()).decode()
 
-server = smtplib.SMTP("smtp.gmail.com", 587) #Connects to gmail servers; port 587 is the main port
-server.startls() #encrypts the connection
+# send email
+send_message = (
+    service.users()
+    .messages()
+    .send(userId="me", body={"raw": encoded_message})
+    .execute()
+)
 
-server.login(email, "") #logins in on user's behalf with an already created app password from google
-
-server.sendmail(email, receiver_email, text) #sends the email
-
-print("Email has been sent to " + receiver_email)
-
+print("Success!")
